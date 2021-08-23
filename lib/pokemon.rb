18eb8c5457e01221433bd3f21de88ecbd18d527d
amy-chinlai <amy@actionnetwork.org>
2021-08-23 14:27:26 -0400

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,15 +1,16 @@
 require_relative "../bin/environment.rb"
 
 class Pokemon
-    attr_accessor :name, :type, :id
+    attr_accessor :name, :type, :id, :db
     
-    def initialize(id: nil, name:, type:)
+    def initialize(id: nil, name:, type:, db:)
         @id = id
         @name = name
         @type = type
+        @db = db
     end
 
-    def self.save
+    def self.save(name, type, db)
       sql = <<-SQL
         INSERT INTO pokemon (name, type)
         VALUES (?, ?)
